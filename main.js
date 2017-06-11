de4b08fe4114a63b684232add446f0831f5f7858
Matt McDonald <mattmcd1221@gmail.com>
2017-06-11 12:14:46 -0400

    create html, add styles declare id's

---
+++ main.js
@@ -0,0 +1,39 @@
+let keyZero = document.getElementById('zero');
+let keyOne = document.getElementById('one');
+let keyTwo = document.getElementById('two');
+let keyThree = document.getElementById('three');
+let keyFour = document.getElementById('four');
+let keyFive = document.getElementById('five');
+let keySix = document.getElementById('six');
+let keySeven = document.getElementById('seven');
+let keyEight = document.getElementById('eight');
+let keyNine = document.getElementById('nine');
+
+let divide = document.getElementById('divide');
+let multiply = document.getElementById('multiply');
+let subtract = document.getElementById('subtract');
+let addition = document.getElementById('addition');
+
+let keyClear = document.getElementById('clear');
+let displayBlock = document.getElementById('display-block');
+let keyDecimal = document.getElementById('decimal');
+let equals = document.getElementById('equals');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(keyOne);
