712a0393f290c15897a2ba9bc0617186f55585b8
Matt McDonald <mattmcd1221@gmail.com>
2017-06-16 09:08:03 -0400

    final commit changes

--- main.js
+++ main.js
@@ -28,26 +28,26 @@ function runNine() {
   document.calculator.result.value += 9;
 }
 function runZero() {
-  document.calculator.result.value = 0;
+  document.calculator.result.value += 0;
 }
 function runDecimal() {
-  document.calculator.result.value = ".";
+  document.calculator.result.value += ".";
 }
 
 
 //Operation functions
 
 function runDivide() {
-  document.calculator.result.value += "/";
+  document.calculator.result.value += " / ";
 }
 function runAddition() {
-  document.calculator.result.value += "+";
+  document.calculator.result.value += " + ";
 }
 function runSubtract() {
-  document.calculator.result.value += "-";
+  document.calculator.result.value += " - ";
 }
 function runMultiply() {
-  document.calculator.result.value += "*";
+  document.calculator.result.value += " * ";
 }
 function runClear() {
   document.calculator.result.value = "";
@@ -56,27 +56,30 @@ function runClear() {
 //Evaluate function
 
 function runEvaluate() {
-  var evaluate = eval(document.calculator.result.value);
+  let evaluate = eval(document.calculator.result.value);
 
   document.calculator.result.value = evaluate;
 
 }
 
-var equals = document.getElementById("equals");
-
-equals.addEventListener('keypress', function checkEnter(e) {
-  if (e.keycode == 13) {
-    evaluate = eval(document.calculator.result.value);
-
-    document.calculator.result.value = evaluate;
-
-  }
-})
-
+// var equals = document.getElementById("equals");
+//
+// equals.addEventListener('keypress', function checkEnter(e) {
+//   if (e.keycode == 13) {
+//     runEvaluate(e);
+//     // evaluate = eval(document.calculator.result.value);
+//
+//     // document.calculator.result.value = evaluate;
+//     console.log(e);
+//
+//   }
+// })
+//
 var one = document.getElementById('one');
 
 one.addEventListener('keypress', function checkOne(e) {
-  if (e.keycode == 49) {
+  if (e.keycode === 49) {
+    // document.calculator.result.value += 1;
     runOne();
   }
 })
